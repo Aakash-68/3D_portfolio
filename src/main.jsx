30882995aab62bccd060e1bcc5cbb9d76406dfbe
Aakash <aakash.yogabalu@gmail.com>
2026-04-11 23:28:36 -0300

    Gh-pages update

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import Test from './game/Test.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/3D_portfolio/t">
+    <BrowserRouter basename="/3D_portfolio/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/t" element={<Test />} />
