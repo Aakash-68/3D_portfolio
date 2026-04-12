8841000de6917ad021bf1061de368df2913c754c
Aakash <aakash.yogabalu@gmail.com>
2026-04-11 23:34:54 -0300

    Gh-pages update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter , Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './css/index.css';
 import App from './App.jsx';
 import Home from './pages/home.jsx';
@@ -9,14 +9,13 @@ import Test from './game/Test.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter  basename="/3D_portfolio/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/t" element={<Test />} />
-        <Route path="/t2" element={<t/>} />
         <Route path="/home" element={<Home />} />
         <Route path="/fin" element={<Fin />} />
       </Routes>
-    </HashRouter  >
+    </HashRouter >
   </StrictMode>
-);
+)
