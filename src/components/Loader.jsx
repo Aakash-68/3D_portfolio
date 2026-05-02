162bd0e5a5c6f896bb90daae13b03dbf97a11fbd
Aakash <aakash.yogabalu@gmail.com>
2026-05-02 12:32:00 -0300

    Fade effect to loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,6 +8,8 @@ export default function Loader() {
       <img src={clouds} className="clouds clouds--back" alt="clouds" />
       <img src={clouds} className="clouds clouds--front" alt="clouds" />
 
+      <img src={clouds} class="clouds clouds--front delay2" />
+      <img src={clouds} class="clouds clouds--front delay3" />
       <img src={plane} className="plane" alt="plane" />
     </div>
   );
